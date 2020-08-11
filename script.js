2bc1d5b932a00668887fc124c2450314f6c3ff9d
Mitch <mitch7w@gmail.com>
2020-08-11 23:13:17 +0200

    Completely overhaul logic using strings and string parsing. Code works for single-digit numbers

--- script.js
+++ script.js
@@ -1,63 +1,153 @@
-let num1 = 0;
-let num2 = 0;
-let operation = "=";
-let currentNum = 0;
-let num1Set = false;
-let screen = "";
+let algebraString = "";
+let numbers = [];
+let operators = [];
+document.getElementById("screen").innerHTML = "0";
+document.getElementById("answerBox").innerHTML = "0";
 
-// When x / + - pressed
-function operate(op) {
-    if (num1Set == true) { // second term
-        num2 = currentNum;
-        num1Set = false;
-        calculate();
-    } else { // first term
-        num1Set = true;
-        num1 = currentNum;
-        currentNum = 0;
-        operation = op ;
-        update();
-    }
+
+function numPressed(no) {
+    algebraString += no;
+    updateScreen();
+}
+
+function clearCalc() {
+    algebraString = "";
+    updateScreen();
+}
+
+function updateScreen() {
+    document.getElementById("screen").innerHTML = algebraString;
+    calculate();
 }
 
 function calculate() {
-    switch (operation) {
-        case "times":
-            num1 = num1 * num2;
-            break;
-        case "divide":
-            num1 = num1 / num2;
-            break;
-        case "plus":
-            num1 = num1 + num2;
-            break;
-        case "minus":
-            num1 = num1 - num2;
-            break;
-        default:
-            console.log("Should never run");
+    sortAlgebra();
+    // Now actually do calculation
+    let total = numbers[0];
+    // decide on operator
+    let counter = 0;
+    while (counter < operators.length) {
+        if (numbers[counter + 1]) {
+            switch (operators[counter]) {
+                case "x":
+                    total *= numbers[counter + 1];
+                    break;
+                case "&#247": // divide
+                    total /= numbers[counter + 1];
+                    break;
+                case "+":
+                    total += numbers[counter + 1];
+                    break;
+                case "-":
+                    total -= numbers[counter + 1];
+                    break;
+            }
+        }
+        counter++;
     }
-    num2 = 0;
-    operation = "=";
-    currentNum = num1;
-    //screen = currentNum ;
+    // update answerBlock
+    document.getElementById("answerBox").innerHTML = total;
 }
 
-function clear() {
-    num1 = 0;
-    num2 = 0;
-    operation = "=";
-    currentNum = 0;
-    num1Set = false;
-    screen = "";
+// Sort into array of numbers and operations
+function sortAlgebra() {
+    numbers.length = 0 ;
+    operators.length=0 ;
+    let numString = "";
+    for (let i = 0; i < algebraString.length; i++) {
+        if (algebraString[i] != "x" && algebraString[i] != "&#247" && algebraString[i] != "+" && algebraString[i] != "-" && algebraString[i] != "=") {
+            numString += algebraString[i];
+        } else { // Have reached an operator
+            numbers.push(parseInt(numString));
+            numString = "";
+            if (algebraString[i] != "=") {
+                operators.push(algebraString[i]);
+            }            
+        }
+    }
+    // If two operators put next to each other disable click of all buttons except CE
 }
 
-function numPressed(no) {
-    screen += no;
-    update();
-}
 
-function update() {
-    currentNum = parseInt("screen");
-    document.getElementById("screen").innerHTML = screen;
+function clearCalc() {
+    algebraString="" ;
+    updateScreen() ;
+    document.getElementById("screen").innerHTML = "0";
+    document.getElementById("answerBox").innerHTML = "0";
 }
+// let num1 = 0;
+// let num2 = 0;
+// let operation = "=";
+// let currentNum = 0;
+// let num1Set = false;
+// let screen = "";
+// let twoPlusTerms = false ;
+
+// // When x / + - pressed
+// function operate(op) {
+//     if (num1Set == true) { // second term
+//         if (twoPlusTerms) {
+//             num2 = currentNum;
+//             operation = op;
+//             calculate();
+//         } else {
+//             num2 = currentNum;
+//             calculate();
+//             operation = op;
+//             twoPlusTerms = true;
+//         }
+//     } else { // first term
+//         num1 = currentNum;
+//         screen = "";
+//         document.getElementById("screen").innerHTML = "";
+//         operation = op;
+//         num1Set = true;
+//     }
+// }
+
+// function calculate() {
+//     switch (operation) {
+//         case "times":
+//             num1 = num1 * num2;
+//             break;
+//         case "divide":
+//             num1 = num1 / num2;
+//             break;
+//         case "plus":
+//             num1 = num1 + num2;
+//             break;
+//         case "minus":
+//             num1 = num1 - num2;
+//             break;
+//         default:
+//             console.log("Should never run. Error!");
+//     }
+//     document.getElementById("screen").innerHTML = num1;
+// }
+
+// function equals() {
+
+// }
+
+// function clearCalc() {
+//     num1 = 0;
+//     num2 = 0;
+//     operation = "=";
+//     currentNum = 0;
+//     num1Set = false;
+//     screen = "";
+//     document.getElementById("screen").innerHTML = "";
+// }
+
+// function numPressed(no) {
+//     screen += no;
+//     update();
+// }
+
+// function update() {
+//     if(twoPlusTerms) {
+//         currentNum = num1;
+//     }
+//     currentNum = parseInt(screen);
+//     document.getElementById("screen").innerHTML = screen;
+// }
